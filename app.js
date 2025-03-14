80890698010604a387e9991b6a4526082380aab2
igor marques <igormrqs12@gmail.com>
2025-03-14 00:29:46 -0300

    Adicao de funcao para adicionar nomes

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 // Array para armazenar os amigos
 let amigos = [];
+
+// Função para adicionar amigo
+function adicionarAmigo() {
+    let nomeInput = document.getElementById("amigo"); // Pega o valor do campo de texto
+    let nome = nomeInput.value.trim(); // Remove espaços extras
+
+    // Se o nome estiver vazio, exibe um alerta
+    if (!nome || nome.trim() === "") {
+        alert("Por favor, insira um nome válido.");
+        return; // Sai da função se não tiver nome
+    }
+
+    amigos.push(nome);
+    
+    atualizarLista();
+
+    nomeInput.value = "";
+}
